9863e83bd5fee3b3b91325757ba1979b12aedae1
devopscicd17 <86373586+devopscicd17@users.noreply.github.com>
2026-06-18 23:11:53 +0530

    Update context file for ZAP DAST script

--- run-zap-dast.sh
+++ run-zap-dast.sh
@@ -8,7 +8,7 @@ mkdir -p reports
 
 zap-baseline.py \
   -t "${TARGET_URL}" \
-  -n  "/zap/wrk/ZapScripts-Context/Authentication Test.context-IBMSSO"
+  -n  "/zap/wrk/ZapScripts-Context/IBM-W3ID-Context.context"
  \
   -U "Authenticated User" \
   -j \
