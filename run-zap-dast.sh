82497bbba3989882c5888097adaff8bb6de10902
devopscicd17 <86373586+devopscicd17@users.noreply.github.com>
2026-06-18 23:26:58 +0530

    Update run-zap-dast.sh

--- run-zap-dast.sh
+++ run-zap-dast.sh
@@ -29,23 +29,4 @@ echo "================================"
 echo "Report generated"
 echo "================================"
 
-ls -lh reports#!/bin/bash
-
-set -e
-
-TARGET_URL="${APP_URL}"
-
-mkdir -p reports
-
-zap-baseline.py \
-  -t "${TARGET_URL}" \
-  -n  "/zap/wrk/ZapScripts-Context/IBM-W3ID-Context.context"
- \
-  -U "Authenticated User" \
-  -j \
-  -I \
-  -r reports/zap-report.html
-
-echo "Report generated"
-
-ls -l reports
+ls -lh reports
