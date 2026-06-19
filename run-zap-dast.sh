4f608e8c0b0afaac535ba0d6b39309c9f7d2287b
devopscicd17 <86373586+devopscicd17@users.noreply.github.com>
2026-06-19 10:58:02 +0530

    Update run-zap-dast.sh

--- run-zap-dast.sh
+++ run-zap-dast.sh
@@ -2,29 +2,21 @@
 
 set -ex
 
-TARGET_URL="https://doc-enrich-frontend-document-enrichment.ae-sap-cluster-test-2322d3421b77f2abeee72f9925acd471-0000.us-south.containers.appdomain.cloud/?teamId=667d2995f046a740e049d6e0&teamName=SAP%20And%20Friends&instance=staging&suite=Delivery"
+git clone https://github.com/devopscicd17/ZapScripts-Context.git
 
-echo "================================"
-echo "Target URL: ${TARGET_URL}"
-echo "================================"
+TARGET_URL="https://doc-enrich-frontend-document-enrichment.ae-sap-cluster-test-2322d3421b77f2abeee72f9925acd471-0000.us-south.containers.appdomain.cloud/"
 
 mkdir -p reports
 
-echo "Repository Contents"
-find /zap/wrk/ZapScripts-Context
+echo "Testing connectivity..."
 
-echo "Testing Context File"
-ls -l "/zap/wrk/ZapScripts-Context/IBM-W3ID-Context.context"
+curl -I "$TARGET_URL" || true
 
 zap-baseline.py \
-  -t "${TARGET_URL}" \
-  -n "/zap/wrk/ZapScripts-Context/IBM-W3ID-Context.context" \
-  -j \
-  -I \
-  -r reports/zap-report.html
-
-echo "================================"
-echo "Report generated"
-echo "================================"
+    -t "$TARGET_URL" \
+    -j \
+    -I \
+    -d \
+    -r reports/zap-report.html
 
 ls -lh reports
